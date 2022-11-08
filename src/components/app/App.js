686a7e45b597a4c6cc353e2c58e53742068dcb77
Hunter McGrew <hunter109@gmail.com>
2022-11-08 14:21:56 -0600

    function to fetchAll api data & saves data in localStorage for now so I don't keep calling the API needlessly.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,11 +10,31 @@ function App() {
 
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [theme, setTheme] = useLocalStorage("theme", defaultDark ? "dark" : "light");
+  const [savedApi, setSavedApi] = useLocalStorage("apiCache", );
+  const [apiAll, setApiAll] = useState();
+
+  console.log("apiAll", apiAll);
+  console.log("savedApi", savedApi);
 
   const switchTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
   }
+
+  const fetchAll = async () => {
+    try {
+      let data = await fetch(`https://restcountries.com/v3.1/all`);
+      let response = await data.json();
+      // console.log("response", response);
+      setApiAll(response);
+      setSavedApi(response);
+    } catch (err) {
+      if (err) throw err;
+    }
+  }
+
+  if (!savedApi) fetchAll();
+  if (savedApi && !apiAll) setApiAll(savedApi);
 
   return (
     <div className="App" data-theme={theme}>

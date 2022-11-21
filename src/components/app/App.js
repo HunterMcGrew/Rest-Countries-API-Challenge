a2cffdc0b27470c121b75d6c2ebb659c3581154c
Hunter <hunter109@gmail.com>
2022-11-21 10:47:55 -0600

    successfully figured out lazy loading react components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,12 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { useState } from 'react';
 import useLocalStorage from 'use-local-storage';
 // components
 import Header from "../header/Header";
 import Search from "../search/Search";
-import Results from '../results/Results';
 import Loading from "../loading/Loading";
+const Results = lazy(() => import("../results/Results.js"));
 
 
 function App() {
@@ -22,20 +22,22 @@ function App() {
     setTheme(newTheme);
   }
   
-  // const fetchAll = async () => {
-  //   try {
-  //     let data = await fetch(`https://restcountries.com/v3.1/all`);
-  //     let response = await data.json();
-  //     // console.log("response", response);
-  //     setApiAll(response);
-  //     setSavedApi(response);
-  //   } catch (err) {
-  //     if (err) throw err;
-  //   }
-  // }
+  const fetchAll = async () => {
+    try {
+      let data = await fetch(`https://restcountries.com/v3.1/all`);
+      let response = await data.json();
+      // console.log("response", response);
+      setApiAll(response);
+      setSavedApi(response);
+    } catch (err) {
+      if (err) throw err;
+    }
+  }
   
-  // if (!savedApi) fetchAll();
-  // if (savedApi && !apiAll) setApiAll(savedApi);
+  // Will probably need to come back to this logic and make sure
+  // it's what i want ******
+  if (!savedApi) fetchAll();
+  if (savedApi && !apiAll) setApiAll(savedApi);
   
   console.log("apiAll", apiAll);
   console.log("savedApi", savedApi);
@@ -51,12 +53,12 @@ function App() {
           theme={theme}
         />
         <Suspense fallback={<Loading />}>
-        <Results
-          theme={theme}
-          apiAll={apiAll}
-          savedApi={savedApi}
+          <Results
+            theme={theme}
+            apiAll={apiAll}
+            savedApi={savedApi}
 
-        />
+          />
         </Suspense>
       </section>
     </section>
--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,22 +1,16 @@
 import React from "react";
-import fetchAll from "../helpers/fetchAll";
 import "./results.css";
-
-const fetchData = fetchAll();
 
 const Results = (props) => {
     console.log("props", props);
-    const data = fetchData.read;
-    console.log("data", data);
 
     return(
 
         <section className="results-container" data-theme={props.theme}>
 
             <div className="flag">
-                {/* <img
-                src={data[0].flags.png}
-                // src={props.apiAll[0].flags.png}
+                <img
+                src={props.apiAll[0].flags.png}
                 />
                 {/* how do i get the SVG from data?
                     needs to be a fetch?
